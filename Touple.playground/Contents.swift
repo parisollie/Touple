aa7f6dcebdd969791eeaa30ac48ef8503e6d4005
parisollie <pjff18@gmail.com>
2025-09-16 21:35:52 -0600

    Initial Commit

---
+++ Touple.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+/**
+ Touple
+ It's something that allows us to group together related pieces of data that have different data types, and it's as convenient as creating a simple dictionary, but it gives you the flexibility of holding different data types and It's not as high effort as creating an entire struct or a class.
+ 
+ So by using a Swift tuple, we're able to organize related pieces of data that are relatively small.
+ 
+ */
+
+let touple1 = ("Angel",12)
+print(touple1.1)
+
+let touple2 = (name:"Carlos",age: 17)
+print(touple2.name)
+
+
+let touple3:(name:String, age: Int)
+touple3 = (name:"Valentina",age: 18)
+print("\(touple3.name) is \(touple3.age) years old.")
+
